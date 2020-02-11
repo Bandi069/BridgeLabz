4c318764f8a2e53ab7b7ed1b4c78eaa50c0de2d6
Bandi069 <bandivenu89@gmail.com>
2020-02-11 18:36:56 +0530

    [Venu]Refactor.Creating Constructor for EagerIntialization.cs Progarm

--- DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/EagerInitialization.cs
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/EagerInitialization.cs
@@ -4,11 +4,40 @@ using System.Text;
 
 namespace DesignPatternPrograms.SingletonPattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class EagerInitialization
     {
-        public void Eagerinitiliaze()
+        private static int Count = 0;
+        private static readonly EagerInitialization NewObject = new EagerInitialization();
+        /// <summary>
+        /// 
+        /// </summary>
+        private EagerInitialization()
         {
-
+            Count++;
+            Console.WriteLine("Counter Value is : " + Count);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static EagerInitialization GetSingleton
+        {
+            get
+            {
+                return NewObject;
+            }
+        }
+
+        /// <summary>
+        /// Method to specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
     }
 }
