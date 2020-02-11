c52db07386e03b6f28cce0c7bed0f9a2a3b18a7d
Bandi069 <bandivenu89@gmail.com>
2020-02-11 17:58:59 +0530

    [Venu]add.Adding Thread-safe singleton program

---
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/ThreadSafeSingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.SingletonPattern
+{
+    class ThreadSafeSingleton
+    {
+        private static int Counter = 0;
+        private static readonly object instancelock = new object();
+        private ThreadSafeSingleton()
+        {
+            Counter++;
+            Console.WriteLine("CounterVAlue is:"+Counter);
+        }
+    }
+}
