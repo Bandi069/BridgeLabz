c5a00fbc7adf856020f0a800b56e60c537d9e38e
Bandi069 <bandivenu89@gmail.com>
2020-02-11 15:33:29 +0530

    [Venu]add.adding Singleton design Pattern Operations Program

---
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/SingletonOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.SingletonPattern
+{
+    class SingletonOperations
+    {
+        public void OperationsOfSingleton()
+        {
+            //// Opeartions of Singleton
+            Console.WriteLine("1. Eager Initilalization Operation");
+            Console.WriteLine("************************\n");
+            Console.WriteLine("Enter a require Oepration to be Perform");
+            int OperationNumber = int.Parse(Console.ReadLine());
+            switch (OperationNumber)
+            {
+                case 1:
+                    EagerInitialization EagerObj = new EagerInitialization();
+                    EagerObj.Eagerinitiliaze();
+                    break;
+            }
+        }
+    }
+}
