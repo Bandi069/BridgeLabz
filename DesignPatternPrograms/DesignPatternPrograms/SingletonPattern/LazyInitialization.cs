ca39c6989e2c9e97a076a2f6418e08c5d550ac8a
Bandi069 <bandivenu89@gmail.com>
2020-02-12 19:08:25 +0530

    [Venu]Refactor.Created Get instance for returning value

--- DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/LazyInitialization.cs
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/LazyInitialization.cs
@@ -18,18 +18,37 @@ namespace DesignPatternPrograms.SingletonPattern
         /// <summary>
         /// Creating Object for Lazy Intialiazation
         /// </summary>
-        private static readonly Lazy<LazyInitialization> SingletonObj = 
+        private static readonly Lazy<LazyInitialization> LazySingletonObj =
             new Lazy<LazyInitialization>(() => new LazyInitialization());
         /// <summary>
         /// Created integer Variable
         /// </summary>
         private static int Count = 0;
         /// <summary>
+        /// returning the singleton value
+        /// </summary>
+        public static LazyInitialization GetSingletonLazy
+        {
+            get
+            {
+                return LazySingletonObj.Value;
+            }
+        }
+        /// <summary>
         /// Creating constructor for the lazyinitialization Class
         /// </summary>
         private LazyInitialization()
         {
-
+            Count++;
+            Console.WriteLine("Counter Value : "+Count);
+        }
+        /// <summary>
+        /// Method for Displaying the message 
+        /// </summary>
+        /// <param name="message"></param>
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
