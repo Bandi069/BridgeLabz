c57829787edcc5f0c8906dba9b0b638f78ca8b0b
Bandi069 <bandivenu89@gmail.com>
2020-02-12 19:14:49 +0530

    [Venu]Refactor.Added switch case for calling the LazyInitilaztion class

--- DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/SingletonOperations.cs
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/SingletonOperations.cs
@@ -14,21 +14,23 @@ namespace DesignPatternPrograms.SingletonPattern
                 //// Opeartions List of Singleton
                 Console.WriteLine("1. Eager Initilalization singleton Operation");
                 Console.WriteLine("2. Thread-Safe Singleton Operation");
+                Console.WriteLine("3. Songleton Lazy initialization OOpeartion");
                 Console.WriteLine("************************\n");
                 Console.WriteLine("Enter a require Oepration to be Perform");
                 int OperationNumber = int.Parse(Console.ReadLine());
                 switch (OperationNumber)
                 {
                     case 1:
-                        EagerInitialization EagerObj = new EagerInitialization();
+                        EagerInitialization EagerObj = EagerInitialization.GetSingleton;
                         EagerObj.Message("Hi,This is Eager");
-                        
-
                         break;
                     case 2:
-                         ThreadSafeSingleton threadSafeObj = new ThreadSafeSingleton();
+                        ThreadSafeSingleton threadSafeObj = ThreadSafeSingleton.GetThreadSingleton;
                         threadSafeObj.Message("Hi,This is Threadsafe Singleton");
-
+                        break;
+                    case 3:
+                        LazyInitialization LazySingleObj = LazyInitialization.GetSingletonLazy;
+                        LazySingleObj.Message("Hi,This is Lazy Singleton Design Pattern");
                         break;
                 }
             }
@@ -37,11 +39,6 @@ namespace DesignPatternPrograms.SingletonPattern
                 Console.WriteLine("Exception occurs" + e.Message);
             }
         }
-        public static void EagerSingleton()
-        {
-            EagerInitialization singletonObject = EagerInitialization.GetSingleton;
-            singletonObject.Message("Message from Eager Singletone");
-        }
-
+      
     }
 }
