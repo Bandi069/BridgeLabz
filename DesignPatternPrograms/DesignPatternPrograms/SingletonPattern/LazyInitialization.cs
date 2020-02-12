dd9bab1b168da59012b03b02566cef3ac06a7bb1
Bandi069 <bandivenu89@gmail.com>
2020-02-12 18:57:43 +0530

    [Venu]Refactor.Created object for Lazy Singleton Program

--- DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/LazyInitialization.cs
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/LazyInitialization.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LazyInitialization.cs" company="Bridgelabz">
+// Copyright © 2019  Company="BridgeLabz"
+// </copyright>
+// <creator name="Bandi Venu"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DesignPatternPrograms.SingletonPattern
 {
+    /// <summary>
+    /// LazyInitialization Class of Singleton Design Pattern
+    /// </summary>
     class LazyInitialization
     {
+        /// <summary>
+        /// Creating Object for Lazy Intialiazation
+        /// </summary>
         private static readonly Lazy<LazyInitialization> SingletonObj = 
             new Lazy<LazyInitialization>(() => new LazyInitialization());
 
