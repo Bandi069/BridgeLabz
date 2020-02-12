171d01ad781c2d7a8837de74864a5c67dd20ca35
Bandi069 <bandivenu89@gmail.com>
2020-02-12 18:54:01 +0530

    [Venu]add.Adding Lazy Singleton Program

---
+++ DesignPatternPrograms/DesignPatternPrograms/SingletonPattern/LazyInitialization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.SingletonPattern
+{
+    class LazyInitialization
+    {
+        private static readonly Lazy<LazyInitialization> SingletonObj = 
+            new Lazy<LazyInitialization>(() => new LazyInitialization());
+
+    }
+}
