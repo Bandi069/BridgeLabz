3893ee6bfbda322cdae2c3d92f962d8c478394cc
Bandi069 <bandivenu89@gmail.com>
2020-02-13 10:50:44 +0530

    [Venu]add.Adding Factory Pattern Program

---
+++ DesignPatternPrograms/DesignPatternPrograms/FactoryPattern/MainFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.FactoryPattern
+{
+    class MainFactory
+    {
+        public void FactoryProduction()
+        {
+            Console.WriteLine("Enter a Choice to get the Details");
+
+            Console.WriteLine("1.PC\n2.Laptop\n3.Server");
+        }
+    }
+}
