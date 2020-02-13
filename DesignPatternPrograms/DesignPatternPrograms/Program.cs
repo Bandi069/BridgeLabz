9b06a61ca6e7714eda04559dd6e12e1dc6694fc5
Bandi069 <bandivenu89@gmail.com>
2020-02-13 11:02:15 +0530

    [Venu]Refactor.Create a Object for MainFactory.cs in switch Case

--- DesignPatternPrograms/DesignPatternPrograms/Program.cs
+++ DesignPatternPrograms/DesignPatternPrograms/Program.cs
@@ -1,4 +1,12 @@
-﻿using DesignPatternPrograms.SingletonPattern;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+// Copyright © 2020  Company="BridgeLabz"
+// </copyright>
+// <creator name="Bandi Venu"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+using DesignPatternPrograms.SingletonPattern;
+using DesignPatternPrograms.FactoryPattern;
 using System;
 
 
@@ -6,20 +14,31 @@ namespace DesignPatternPrograms
 {
     class Program
     {
+        /// <summary>
+        /// Main method for Design Pattern Programs
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //// List of Programs
             Console.WriteLine("1. Singleton Design Pattern");
+            Console.WriteLine("2. Factory Design Pattern");
             
             Console.WriteLine("\n**************************");
             Console.WriteLine("Enter a choice for required program to run");
             int Choice = int.Parse(Console.ReadLine());
             switch(Choice)
             {
+                //// Case 1 for Singleton Design Pattern
                 case 1:
                     SingletonOperations SingtonObj = new SingletonOperations();
                     SingtonObj.OperationsOfSingleton();
                     break;
+                case 2:
+                    MainFactory FactoryObj = new MainFactory();
+                    FactoryObj.FactoryProduction();
+                    break;
+                    //// Default case for Invalid choice
                 default:
                     Console.WriteLine("Entered Invalid Choice...Enter Valid Choice");
                     break;
