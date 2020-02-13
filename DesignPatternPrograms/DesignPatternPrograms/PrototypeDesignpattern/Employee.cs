8efc040f38a0a8ca4e5d038cd8ea8f1d0f0ac940
Bandi069 <bandivenu89@gmail.com>
2020-02-13 17:31:58 +0530

    [Venu]Add.Adding Prototype Design Pattern Program folder

---
+++ DesignPatternPrograms/DesignPatternPrograms/PrototypeDesignpattern/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.PrototypeDesignpattern
+{
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+    }
+}
