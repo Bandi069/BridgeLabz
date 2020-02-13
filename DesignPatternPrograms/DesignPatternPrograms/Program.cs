a329beeae001724afba985cdd3e8409eda4304c0
Bandi069 <bandivenu89@gmail.com>
2020-02-13 17:39:28 +0530

    [Venu]Refactor.created object for Prototype design pattern in main method

--- DesignPatternPrograms/DesignPatternPrograms/Program.cs
+++ DesignPatternPrograms/DesignPatternPrograms/Program.cs
@@ -7,6 +7,7 @@
 
 using DesignPatternPrograms.SingletonPattern;
 using DesignPatternPrograms.FactoryPattern;
+using DesignPatternPrograms.PrototypeDesignpattern;
 using System;
 
 
@@ -23,6 +24,7 @@ namespace DesignPatternPrograms
             //// List of Programs
             Console.WriteLine("1. Singleton Design Pattern");
             Console.WriteLine("2. Factory Design Pattern");
+            Console.WriteLine("3. Proto type Design Pattern");
             
             Console.WriteLine("\n**************************");
             Console.WriteLine("Enter a choice for required program to run");
@@ -38,6 +40,11 @@ namespace DesignPatternPrograms
                 case 2:
                     MainFactory FactoryObj = new MainFactory();
                     FactoryObj.FactoryProduction();
+                    break;
+                    //// ProtoType Design Pattern
+                case 3:
+                    ProtoTypeOperation ProtoObj = new ProtoTypeOperation();
+                    ProtoObj.CloneOperation();
                     break;
                     //// Default case for Invalid choice
                 default:
