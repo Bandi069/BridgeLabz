217b7d9abf7ad43ae0b7c1004abafea0d50ed654
Bandi069 <bandivenu89@gmail.com>
2020-02-13 10:56:50 +0530

    [Venu]Refactor.Implemented Switch case for calling diffferentmethods

--- DesignPatternPrograms/DesignPatternPrograms/FactoryPattern/MainFactory.cs
+++ DesignPatternPrograms/DesignPatternPrograms/FactoryPattern/MainFactory.cs
@@ -9,8 +9,16 @@ namespace DesignPatternPrograms.FactoryPattern
         public void FactoryProduction()
         {
             Console.WriteLine("Enter a Choice to get the Details");
-
             Console.WriteLine("1.PC\n2.Laptop\n3.Server");
+            int Choice = int.Parse(Console.ReadLine());
+            switch(Choice)
+            {
+                case 1:
+                    break;
+                default:
+                    Console.WriteLine("Entered Invalid Choice...Enter a Valid Choice");
+                    break;
+            }
         }
     }
 }
