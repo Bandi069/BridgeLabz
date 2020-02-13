60ea53e995fee3cc688dc6d24694c0b24f258a9f
Bandi069 <bandivenu89@gmail.com>
2020-02-13 18:02:10 +0530

    [Venu]Refactor.Implemented Clone operations for Employee

--- DesignPatternPrograms/DesignPatternPrograms/PrototypeDesignpattern/ProtoTypeOperation.cs
+++ DesignPatternPrograms/DesignPatternPrograms/PrototypeDesignpattern/ProtoTypeOperation.cs
@@ -6,5 +6,19 @@ namespace DesignPatternPrograms.PrototypeDesignpattern
 {
     class ProtoTypeOperation
     {
+        public void CloneOperation()
+        {
+            Employee EmployeeObj = new Employee();
+            Console.WriteLine("Enter Employee ID ");
+            int EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Employee Name");
+            string EmployeeName = Console.ReadLine();
+            Console.WriteLine("Enter employee Department");
+            string EmployeeDepartment = Console.ReadLine();
+            EmployeeObj.EmployeeName = EmployeeName;
+            EmployeeObj.Department = EmployeeDepartment;
+            EmployeeObj.EmployeeID = EmployeeID;
+        }
+
     }
 }
