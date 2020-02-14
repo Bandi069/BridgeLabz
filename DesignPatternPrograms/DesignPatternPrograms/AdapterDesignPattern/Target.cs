79109ed3b9a35435edfb3f6f7ca16b0d60c156a4
Bandi069 <bandivenu89@gmail.com>
2020-02-14 15:56:46 +0530

    [Venu]Add. Implementation of Adapter Design Pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/AdapterDesignPattern/Target.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.AdapterDesignPattern
+{
+    public interface Target
+    {
+        public List<string> GetProducts();
+    }
+}
