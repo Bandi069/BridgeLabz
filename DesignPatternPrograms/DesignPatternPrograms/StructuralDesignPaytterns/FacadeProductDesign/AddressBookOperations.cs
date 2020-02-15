afda1323f759e3351e53a678db00ef4cbe3486da
Bandi069 <bandivenu89@gmail.com>
2020-02-15 12:02:43 +0530

    [Venu]Refactor.Implementation of Methods and Added Comments to Program

--- DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/AddressBookOperations.cs
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/AddressBookOperations.cs
@@ -4,13 +4,28 @@ using System.Text;
 
 namespace DesignPatternPrograms.StructuralDesignPaytterns.FacadeProductDesign
 {
-    public interface AddressBookOperations
+    /// <summary>
+    /// This is Address book Operations Interface Class
+    /// </summary>
+    public interface FacadeAddressBookOperations
     {
+        /// <summary>
+        /// This is Method for getting Address Book user Details
+        /// </summary>
         public void UserDetails();
-
+        /// <summary>
+        /// This is Method for Adding the user to Address Book
+        /// </summary>
         public void AddUser();
-
+        /// <summary>
+        /// This is method for Update the user details in Address Book
+        /// </summary>
         public void UpdateUSer();
+        /// <summary>
+        /// This is method for Removing user from Address Book
+        /// </summary>
+        public void RemoveUser();
+
 
 
     }
