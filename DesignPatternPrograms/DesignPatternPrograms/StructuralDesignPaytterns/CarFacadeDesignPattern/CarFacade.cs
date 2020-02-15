260b8a452abdb43679a3ed04fffb7bfeb5088617
Bandi069 <bandivenu89@gmail.com>
2020-02-15 15:19:44 +0530

    [Venu]Add.Adding Facade car class

---
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/CarFacadeDesignPattern/CarFacade.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatternPrograms.StructuralDesignPaytterns.ProductFacadeDesignPattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPaytterns.CarFacadeDesignPattern
+{
+   public class CarFacade
+    {
+        CarModel ModelObj = new CarModel();
+            CarBody BodyObj = new CarBody();
+            CarEngine EngineObj = new CarEngine();
+        public void carfade()
+        {
+            ModelObj.Model();
+            BodyObj.Body();
+            EngineObj.Engine();
+        } 
+      
+      
+    }
+}
