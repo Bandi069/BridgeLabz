a916241491e6caac58ef81f6869252fcbd7582c6
Bandi069 <bandivenu89@gmail.com>
2020-02-15 12:20:00 +0530

    [Venu]Refactor.added comments to FacadeaddressBookdetails program

--- DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/FacadeAddressBookDetails.cs
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/FacadeAddressBookDetails.cs
@@ -4,13 +4,46 @@ using System.Text;
 
 namespace DesignPatternPrograms.StructuralDesignPaytterns.FacadeProductDesign
 {
+    /// <summary>
+    /// Interface for the Addressbook
+    /// </summary>
     public interface  FacadeAddressBookDetails
     {
+        /// <summary>
+        /// Method Instance of FirstName
+        /// </summary>
+        /// <returns></returns>
         public string FirstName();
-        public string LastNAme();
+        /// <summary>
+        /// This method is Instance for LastName
+        /// </summary>
+        /// <returns></returns>
+        public string LastName();
+        /// <summary>
+        /// Method for Instance of Village
+        /// </summary>
+        /// <returns></returns>
         public string Village();
+        /// <summary>
+        /// This is method for City
+        /// </summary>
+        /// <returns></returns>
         public string City();
+        /// <summary>
+        /// This is MEthod for State
+        /// </summary>
+        /// <returns></returns>
         public string State();
+        /// <summary>
+        /// This is Method for Zipcode
+        /// </summary>
+        /// <returns></returns>
+        public int Zipcode();
+        /// <summary>
+        /// This is MEthod for Phone Number
+        /// </summary>
+        /// <returns></returns>
+        public int PhoneNumber();
 
     }
 }
