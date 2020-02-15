46f625703973b5d81fc12bc7fdcf805021839fb4
Bandi069 <bandivenu89@gmail.com>
2020-02-15 12:05:59 +0530

    [Venu]Add.Adding Address Book Details Program

---
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/FacadeAddressBookDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPaytterns.FacadeProductDesign
+{
+    public interface  FacadeAddressBookDetails
+    {
+        public string FirstName();
+
+    }
+}
