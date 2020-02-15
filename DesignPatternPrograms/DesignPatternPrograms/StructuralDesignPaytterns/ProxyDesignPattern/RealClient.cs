2b549e29ff8cd8870c5109cda1b226029db41a7c
Bandi069 <bandivenu89@gmail.com>
2020-02-15 18:09:33 +0530

    [Venu]Add. Adding Real client class in Proxy Design Pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/RealClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPaytterns.ProxyDesignPattern
+{
+   public class RealClient :Client
+    {
+        string Data;
+        public RealClient()
+        {
+            Console.WriteLine("it is Real Client");
+        }
+    }
+}
