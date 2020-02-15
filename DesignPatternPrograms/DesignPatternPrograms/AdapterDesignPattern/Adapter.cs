c9f4b771a7bde619cbba0ba35766ad7a1cb676ae
Bandi069 <bandivenu89@gmail.com>
2020-02-15 10:49:47 +0530

    [Venu]Refactor.Implemted Adaptor Design Pattern

--- DesignPatternPrograms/DesignPatternPrograms/AdapterDesignPattern/Adapter.cs
+++ DesignPatternPrograms/DesignPatternPrograms/AdapterDesignPattern/Adapter.cs
@@ -7,7 +7,7 @@ namespace DesignPatternPrograms.AdapterDesignPattern
     /// <summary>
     /// This is Adapter Class   
     /// </summary>
-    public class Adapter
+    public class Adaptee
     {
         /// <summary>
         /// String List
@@ -18,19 +18,31 @@ namespace DesignPatternPrograms.AdapterDesignPattern
             List<string> AdapterList = new List<string>();
             Console.WriteLine("Enter Number of adapts");
             int Number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Adaptee List");
+
             for (int i = 0; i < Number; i++)
             {
                 string Adaptee = Console.ReadLine();
+                //// adding Adaptee to the list
                 AdapterList.Add(Adaptee);
             }
+            //// returning adapteList
             return AdapterList;
         }
+        /// <summary>
+        /// Adapter Target Interface Class
+        /// </summary>
         public class Adapter : Target
         {
+            /// <summary>
+            /// Method to get the adaptee List
+            /// </summary>
+            /// <returns></returns>
             public List<string> GetList()
             {
-                Adapter Adaptee = new Adapter();
-                return Adaptee.GetList();
+                //// object for Adaptee
+                Adaptee AdapteeObj = new Adaptee();
+                return AdapteeObj.GetList();
             }
 
         }
