321060a2e384b51804c2d135a0f32c5bb5b53132
Bandi069 <bandivenu89@gmail.com>
2020-02-15 12:37:19 +0530

    [Venu]Refactor.Implemented different methods for User Details of Address book

---
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/FacadeProductDesign/FacadeAddressBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPaytterns.FacadeProductDesign
+{
+    public class FacadeAddressBook 
+    {
+        public string FirstName()
+        {
+            Console.WriteLine("Enter FirstName:");
+            string FirstName = Console.ReadLine();
+            return FirstName;
+        }
+        public string LastName()
+        {
+            Console.WriteLine("Enter LastName:");
+            string LastName = Console.ReadLine();
+            return LastName;
+        }
+        public string Village()
+        {
+            Console.WriteLine("Enter Village:");
+            string Village = Console.ReadLine();
+            return Village;
+        }
+        public string City()
+        {
+            Console.WriteLine("Enter City:");
+            string City = Console.ReadLine();
+            return City;
+        }
+        public string State()
+        {
+            Console.WriteLine("Enter State:");
+            string State = Console.ReadLine();
+            return State;
+        }
+        public int Zipcode()
+        {
+            Console.WriteLine("Enter Zipcode:");
+            int Zipcode = int.Parse(Console.ReadLine());
+            return Zipcode;
+        }
+        public int PhoneNumber()
+        {
+            Console.WriteLine("Enter PhoneNumber:");
+            int PhoneNumber = int.Parse(Console.ReadLine());
+            return PhoneNumber;
+        }
+
+
+    }
+}
