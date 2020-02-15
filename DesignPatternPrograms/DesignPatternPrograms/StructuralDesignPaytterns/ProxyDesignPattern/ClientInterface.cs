84bb2a3a0a35d1a39dabba8eddc5de182cbf288b
Bandi069 <bandivenu89@gmail.com>
2020-02-15 18:03:40 +0530

    [Venu]Add.Implementation of Proxy Design Pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/ClientInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPaytterns.ProxyDesignPattern
+{
+    public interface Client
+    {
+        string GetData();
+    }
+    
+}
