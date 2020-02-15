38a6cb1c4d7999d29454493e0b47a2cc20b247fb
Bandi069 <bandivenu89@gmail.com>
2020-02-15 10:07:10 +0530

    [Venu]Refactor.Implemted interface Class of Adaptee

--- DesignPatternPrograms/DesignPatternPrograms/AdapterDesignPattern/Adapter.cs
+++ DesignPatternPrograms/DesignPatternPrograms/AdapterDesignPattern/Adapter.cs
@@ -13,17 +13,27 @@ namespace DesignPatternPrograms.AdapterDesignPattern
         /// String List
         /// </summary>
         /// <returns></returns>
-        public  List<string> GetList()
+        public List<string> GetList()
         {
-                List<string> AdapterList = new List<string>();
-                Console.WriteLine("Enter Number of adapts");
-                int Number = int.Parse(Console.ReadLine());
-                for (int i = 0; i < Number; i++)
-                {
-                    string Adaptee= Console.ReadLine();
+            List<string> AdapterList = new List<string>();
+            Console.WriteLine("Enter Number of adapts");
+            int Number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < Number; i++)
+            {
+                string Adaptee = Console.ReadLine();
                 AdapterList.Add(Adaptee);
-                }
+            }
             return AdapterList;
         }
+        public override class Adapter : Target
+        {
+            public List<string> GetList()
+            {
+                Adapter Adaptee = new Adapter();
+                return Adaptee.GetList();
+            }
+            
+        }
+    
     }
 }
