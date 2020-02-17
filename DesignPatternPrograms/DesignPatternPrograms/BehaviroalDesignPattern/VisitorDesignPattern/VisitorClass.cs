0cb51abc45316baeb3e525f94cbac2527006d623
Bandi069 <bandivenu89@gmail.com>
2020-02-17 15:57:02 +0530

    [Venu]Refactor.implementing Price method in Visitor Class

--- DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/VisitorClass.cs
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/VisitorClass.cs
@@ -9,6 +9,14 @@ namespace DesignPatternPrograms.BehaviroalDesignPattern.VisitorDesignPattern
         public int Visit(Book book)
         {
             int cost = 0;
+            if(book.GetPrice()>50)
+            {
+                cost = book.GetPrice() - 10;
+            }
+            else
+            {
+
+            }
             return cost;
         }
         public int Visit(Fruit fruit);
@@ -17,9 +25,19 @@ namespace DesignPatternPrograms.BehaviroalDesignPattern.VisitorDesignPattern
     {
         public int Accept(IVisitor visitor);
     }
-    
-    public class Fruit
+    public class ShoppingVisit : IVisitor
     {
+        public int Visit(Book book)
+        {
 
+        }
+
+        public int Visit(Fruit fruit)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
+
 }
