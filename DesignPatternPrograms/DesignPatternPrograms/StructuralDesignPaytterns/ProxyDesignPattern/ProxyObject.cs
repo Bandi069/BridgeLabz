9b80b39427fe33a5edaeaf76f24290498c863183
Bandi069 <bandivenu89@gmail.com>
2020-02-17 12:49:10 +0530

    [Venu]Refactor.Adding comments to ProxyObject.cs

--- DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/ProxyObject.cs
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/ProxyObject.cs
@@ -4,13 +4,26 @@ using System.Text;
 
 namespace DesignPatternPrograms.StructuralDesignPaytterns.ProxyDesignPattern
 {
+    /// <summary>
+    /// This is proxy Object Class
+    /// </summary>
     public class ProxyObject : Client
     {
+        /// <summary>
+        /// Creating object to Real Client Class
+        /// </summary>
         RealClient RealClientObj = new RealClient();
+        /// <summary>
+        /// This is ProxyObject Constructor
+        /// </summary>
         public ProxyObject()
         {
             Console.WriteLine("This is Proxy Object class");
         }
+        /// <summary>
+        /// Method for Returning the GetData value
+        /// </summary>
+        /// <returns></returns>
         public string GetData()
         {
             return RealClientObj.GetData();
