0378360f31a69df62fd2a1cc3a785b33db5f932a
Bandi069 <bandivenu89@gmail.com>
2020-02-17 18:51:42 +0530

    [Venu]Add.Adding Medaitor design pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/MediatorDesignPattern/ChatMediator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.BehaviroalDesignPattern.MediatorDesignPattern
+{
+    public interface ChatMediator
+    {
+        public void Message();
+        public void AddUser();
+    }
+    
+}
