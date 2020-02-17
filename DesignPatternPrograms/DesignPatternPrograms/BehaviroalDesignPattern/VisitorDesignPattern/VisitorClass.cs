5d84249deb69c997bf6a733511ac71d615a70f2e
Bandi069 <bandivenu89@gmail.com>
2020-02-17 15:15:59 +0530

    [Venu]Add.Adding Visitor class in visitor design pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/VisitorClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.BehaviroalDesignPattern.VisitorDesignPattern
+{
+    public interface IVisitor
+    {
+        public int Visit(Book book)
+        {
+            int cost = 0;
+            return cost;
+        }
+        public int Visit(Fruit fruit);
+    }
+    public interface IitemElement
+    {
+        public int Accept(IVisitor visitor);
+    }
+    
+    public class Fruit
+    {
+
+    }
+}
