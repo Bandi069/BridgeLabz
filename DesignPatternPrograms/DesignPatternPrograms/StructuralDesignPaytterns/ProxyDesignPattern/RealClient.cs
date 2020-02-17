41e7118da5096dff0bb45fb32334c8257dbb2953
Bandi069 <bandivenu89@gmail.com>
2020-02-17 12:46:14 +0530

    [Venu]Refactor.Adding comments to RealClient.cs

--- DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/RealClient.cs
+++ DesignPatternPrograms/DesignPatternPrograms/StructuralDesignPaytterns/ProxyDesignPattern/RealClient.cs
@@ -4,15 +4,28 @@ using System.Text;
 
 namespace DesignPatternPrograms.StructuralDesignPaytterns.ProxyDesignPattern
 {
+    /// <summary>
+    /// This is Real Client Class 
+    /// </summary>
    public class RealClient : Client
     {
-        string Data;
+        /// <summary>
+        /// This is Data object giving as Public String 
+        /// </summary>
+        public string Data;
+        /// <summary>
+        /// This is Constructor 
+        /// </summary>
         public RealClient()
         {
             Console.WriteLine("It is Real Client");
             Console.WriteLine("Enter a String");
             Data = Console.ReadLine();
         }
+        /// <summary>
+        /// This is GetData Method for returning Data
+        /// </summary>
+        /// <returns value="Data"></returns>
         public string GetData()
         {
             return Data;
