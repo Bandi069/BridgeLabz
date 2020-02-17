1325af46bc3b4d678b8b12d7d9d3af1cce140161
Bandi069 <bandivenu89@gmail.com>
2020-02-17 16:29:49 +0530

    [Venu]Refactor.Implemented new method for calculations

--- DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/Shopping.cs
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/Shopping.cs
@@ -6,5 +6,26 @@ namespace DesignPatternPrograms.BehaviroalDesignPattern.VisitorDesignPattern
 {
     class Shopping
     {
+        public  void MethodShopping()
+        {
+
+            IitemElement[] items = new IitemElement[]
+            {
+                new Book(20,"1234"),
+                new Fruit(5,10,"Banana")
+            };
+            int total = CalculatePrice(items);
+            Console.WriteLine("Total Price" + total);
+        }
+        public static int CalculatePrice(IitemElement[] titems)
+        {
+            IVisitor VisitObj = new Visit();
+            int totalprice = 0;
+            foreach(ItemElements items in items)
+            {
+                totalprice = totalprice + items.Acceppt(IVisitor);
+            }
+            return cost;
+        }
     }
 }
