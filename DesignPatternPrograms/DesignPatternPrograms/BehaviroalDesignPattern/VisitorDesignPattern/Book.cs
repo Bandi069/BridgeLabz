4c6cbad23e2e1b4e7348c21d86c0661c11201437
Bandi069 <bandivenu89@gmail.com>
2020-02-17 15:16:21 +0530

    [Venu]Add.Adding Book class in visitor design pattern

---
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/VisitorDesignPattern/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.BehaviroalDesignPattern.VisitorDesignPattern
+{
+   public class Book
+    {
+        private int Price;
+        private string isbnNumber;
+        public Book(int cost,string isbnNumber)
+        {
+            this.Price = cost;
+            this.isbnNumber = isbnNumber;
+        }
+
+    }
+}
