35e3e7482e2c18f41ec2e7ec66586e110e7043b5
Bandi069 <bandivenu89@gmail.com>
2020-02-18 09:40:46 +0530

    [Venu]Add.Implementing Client Chat class for Mediator design apttern Program

---
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/MediatorDesignPattern/ChatClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.BehaviroalDesignPattern.MediatorDesignPattern
+{
+    class ChatClient
+    {
+        public void Chat()
+        {
+
+        }
+    }
+}
