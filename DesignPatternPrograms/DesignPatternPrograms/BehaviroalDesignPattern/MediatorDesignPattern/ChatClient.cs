1309d695b449da52a0f20aadcd49cf9daf1efd66
Bandi069 <bandivenu89@gmail.com>
2020-02-18 09:45:06 +0530

    [Venu]Refactor.Implementing users in ChatClient class

--- DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/MediatorDesignPattern/ChatClient.cs
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/MediatorDesignPattern/ChatClient.cs
@@ -9,6 +9,9 @@ namespace DesignPatternPrograms.BehaviroalDesignPattern.MediatorDesignPattern
         public void Chat()
         {
             ChatMediator MediatorObj = new ChatMediatorImpl();
+            UserMediator user1 = new UserImpl(MediatorObj, "Pankaj");
+            UserMediator user2 = new UserImpl(MediatorObj, "Lisa");
+            UserMediator user3 = new UserImpl(MediatorObj, "Saurabh");
 
         }
     }
