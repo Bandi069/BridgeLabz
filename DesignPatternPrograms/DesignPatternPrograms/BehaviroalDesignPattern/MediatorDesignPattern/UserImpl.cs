58f4f6cec175a7d46427c8d800c9e15d8a8fcba7
Bandi069 <bandivenu89@gmail.com>
2020-02-18 09:39:42 +0530

    [Venu]Add.Implementing user sending and recieving message methods

---
+++ DesignPatternPrograms/DesignPatternPrograms/BehaviroalDesignPattern/MediatorDesignPattern/UserImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.BehaviroalDesignPattern.MediatorDesignPattern
+{
+    public class UserImpl : UserMediator
+    {
+        public string name;
+
+        public UserImpl(ChatMediator msg, string name) : base(msg, name) { }
+       public void send(string msg)
+        {
+            Console.WriteLine(this.name +"Messge sending "+msg);
+        }
+        public void receive(string msg)
+        {
+            Console.WriteLine(this.name + "Recieve messgae :" + msg);
+        }
+    }
+}
