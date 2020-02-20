e8997e919718a41867f96713dae299ce216f8766
Bandi069 <bandivenu89@gmail.com>
2020-02-20 15:20:45 +0530

    [BandiVenu]Add.Implementation of Employee Management Program

--- EmployeeManagement/EmployeeManagement/Utility.cs
+++ EmployeeManagement/EmployeeManagement/Utility.cs
@@ -4,21 +4,23 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
 
 namespace EmployeeManagement
 {
-    public class Utility
+    public class Utility    
     {
         public static void Connection()
         {
-            string ConnectionName = "database=.;databse=EmployeeManagement;integrated Security=SSPI";
+           // string ConnectionName = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+           string ConnectionName = "database=.;databse=EmployeeManagement;integrated Security=SSPI";
             SqlConnection connect = new SqlConnection(ConnectionName);
             try
             {
                 SqlCommand cmd = new SqlCommand("SELECT *from Employee", connect);
                 connect.Open();
                 cmd.ExecuteReader();
-                
             }
             catch
             {
