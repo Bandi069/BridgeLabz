3a7ce87b72cbdcaab4e2be149b43ba02a8f352bd
Bandi069 <bandivenu89@gmail.com>
2020-02-20 12:05:57 +0530

    [BandiVenu]Add.Implementing Employee Management Program

---
+++ EmployeeManagement/EmployeeManagement/Models/ModelClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class ModelClass : ModelInterface
+    {
+        public int EmpplyeeID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmaidlID { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string EmployeeAddress { get; set; }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EmployeeManagement
+{
+    public class Utility
+    {
+        public static void Connection()
+        {
+            string ConnectionName = "database=.;databse=EmployeeManagement;integrated Security=SSPI";
+            SqlConnection connect = new SqlConnection(ConnectionName);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT *from Employee", connect);
+                connect.Open();
+                cmd.ExecuteReader();
+                
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occurs");
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+    }
+}
