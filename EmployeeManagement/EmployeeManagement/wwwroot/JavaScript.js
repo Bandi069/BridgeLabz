6256d05fc896404cc4902247b1025f6b91ebe434
Bandi069 <bandivenu89@gmail.com>
2020-02-24 18:23:09 +0530

    [BandiVenu]Refactor.Implementation of Html,Css,JavaScript files

--- EmployeeManagement/EmployeeManagement/wwwroot/JavaScript.js
+++ EmployeeManagement/EmployeeManagement/wwwroot/JavaScript.js
@@ -3,11 +3,18 @@
 }
 function validate() {
     var Firstname = document.getElementsByName("FirstName");
+    var phonenumber = document.getElementsByName("PhoneNumber");
     if (Firstname.value == "") {
         alert("Empty Not allowed");
         return false;
     }
     else {
         return true;
+    }
+    if (phonenumber.value.trim().lenth == 10) {
+        return false;
+    }
+    else {
+        alert("Invalid Phone Number")
     }
 }
