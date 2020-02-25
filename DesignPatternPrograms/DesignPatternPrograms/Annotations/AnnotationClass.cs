b16428575e825344c3dfd4950cf24d3996200f77
Bandi069 <bandivenu89@gmail.com>
2020-02-25 19:04:21 +0530

    [Venu]Add.Adding Annotations Programs

---
+++ DesignPatternPrograms/DesignPatternPrograms/Annotations/AnnotationClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.Annotations
+{
+    class AnnotationClass
+    {
+        public void Annotation()
+        {
+            Console.WriteLine();
+        }
+    }
+}
