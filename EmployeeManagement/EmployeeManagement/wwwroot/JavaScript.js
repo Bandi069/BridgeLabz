18240c983e958954d6c6bfdd336f51d763d59145
Bandi069 <bandivenu89@gmail.com>
2020-02-25 14:49:39 +0530

    [BandiVenu]Refactor.Modified JavaScript File form validations

--- EmployeeManagement/EmployeeManagement/wwwroot/JavaScript.js
+++ EmployeeManagement/EmployeeManagement/wwwroot/JavaScript.js
@@ -1,20 +1,49 @@
 ﻿function Clickfunction() {
     alert("Registered Succesfully");
 }
-function validate() {
+
+
+function validateForm() {
+    var x = document.forms["FormValidation"]["FirstName"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+function registration(names) {
+    var feilds = document.getElementsByClassName("Registration");
+    if (feilds.test(names.value == "")) {
+        alert("Empty feilds not Accepted");
+        return false;
+    }
+    return true;
+}
+function FirstNamevalidate(Firstname) {
     var Firstname = document.getElementsByName("lineclass");
-    var phonenumber = document.getElementsByName("PhoneNumber");
     if (Firstname == "") {
-        alert("Empty Not allowed");
+        alert("First Name Empty Not allowed");
         return false;
     }
     else {
         return true;
     }
-    if (phonenumber.value.trim().length == 10) {
+
+}
+function ValidatePhonenumber(Phonenumber) {
+    var reg = /^([7-9]{1}[0-9]{9})$/;
+    if (reg.test(Phonenumber.value) == false) {
+        console.log(reg.test(Phonenumber.value));
+        alert("Invalid Phone NUmber");
         return false;
     }
-    else {
-        alert("Invalid Phone Number")
+    return true;
+}
+function EmailIdValidateion(emailField) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (reg.test(emailField.value) == false) {
+        alert('Invalid Email Address');
+        return false;
     }
+
+    return true;
 }
