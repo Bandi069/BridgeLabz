6024c336c000d54f87ea81df6480930b94a663fe
Bandi069 <bandivenu89@gmail.com>
2020-02-26 09:59:08 +0530

    [Venu]Refactor.Adding error messages to EmployeeAnnotation class

--- DesignPatternPrograms/DesignPatternPrograms/Annotations/EmployeeAnnotations.cs
+++ DesignPatternPrograms/DesignPatternPrograms/Annotations/EmployeeAnnotations.cs
@@ -8,14 +8,17 @@ namespace DesignPatternPrograms.Annotations
     class EmployeeAnnotations
     {
         public void Employee()
-        {
-            
+        {            
             AnnotationClass annotationObj = new AnnotationClass();
             annotationObj.EmployeeName = "";
             var context = new ValidationContext(annotationObj,null);
             var validationresult = new List<ValidationResult>();
-            bool BoolValid = Validator.TryValidateObject(annotationObj, context,ValidationResult,true);
+            bool BoolValid = Validator.TryValidateObject(annotationObj, context, validationresult, true);
             Console.WriteLine(BoolValid);
+            foreach(var errorresult in validationresult)
+            {
+                Console.WriteLine(errorresult.ErrorMessage);
+            }
         }
     }
 }
