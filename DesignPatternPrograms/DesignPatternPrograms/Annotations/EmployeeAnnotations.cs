0a4a0b88549e1de118c0e44fdd76d399b08be17c
Bandi069 <bandivenu89@gmail.com>
2020-02-26 09:55:21 +0530

    [Venu]Refactor.Implementing validations EmployeeAnnotation class

--- DesignPatternPrograms/DesignPatternPrograms/Annotations/EmployeeAnnotations.cs
+++ DesignPatternPrograms/DesignPatternPrograms/Annotations/EmployeeAnnotations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DesignPatternPrograms.Annotations
@@ -10,7 +11,11 @@ namespace DesignPatternPrograms.Annotations
         {
             
             AnnotationClass annotationObj = new AnnotationClass();
-
+            annotationObj.EmployeeName = "";
+            var context = new ValidationContext(annotationObj,null);
+            var validationresult = new List<ValidationResult>();
+            bool BoolValid = Validator.TryValidateObject(annotationObj, context,ValidationResult,true);
+            Console.WriteLine(BoolValid);
         }
     }
 }
