ce4eab8a540b007d9e9e8ccfb9be5c4d19e7bfe5
Bandi069 <bandivenu89@gmail.com>
2020-02-26 09:40:43 +0530

    [Venu]Add.Adding EmployeeAnnotation class

---
+++ DesignPatternPrograms/DesignPatternPrograms/Annotations/EmployeeAnnotations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.Annotations
+{
+    class EmployeeAnnotations
+    {
+        public void Employee()
+        {
+            
+            AnnotationClass annotationObj = new AnnotationClass();
+
+        }
+    }
+}
