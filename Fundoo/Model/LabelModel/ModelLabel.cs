e5d9019989da1a3f76b32c808f6da068bf7ef0b4
Bandi069 <bandivenu89@gmail.com>
2020-03-04 13:15:13 +0530

    [Bandi Venu]Add.Adding Fundoo file

---
+++ Fundoo/Model/LabelModel/ModelLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.LabelModel
+{
+    public class ModelLabel
+    {
+        public int MyProperty { get; set; }
+        public string EmailId { get; set; }
+        public string Label { get; set; }
+    }
+}
