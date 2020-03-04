d91e937407ac20d76644157250157cfea7ee54bb
Bandi069 <bandivenu89@gmail.com>
2020-03-04 15:42:52 +0530

    [Venu]Refactor.Implementation of model class UserRegistration

--- Fundoo/Model/UserModel/UserRegistration.cs
+++ Fundoo/Model/UserModel/UserRegistration.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Model.UserModel
 {
     public class UserRegistration
     {
+        [Required]
+        public string UserName{ get; set; }
+        [Required]
+        public string EmailID { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }
