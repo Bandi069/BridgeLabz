322fb130cce23f7508096e50a5f69254a6c9c102
Bandi069 <bandivenu89@gmail.com>
2020-03-11 13:19:30 +0530

    [Venu]Refactor.Implementation of AccountManager class in Manager

--- Fundoo/Manager/InterfaceManager/IAccountManager.cs
+++ Fundoo/Manager/InterfaceManager/IAccountManager.cs
@@ -6,14 +6,53 @@ using System.Threading.Tasks;
 
 namespace Manager.InterfaceManager
 {
+    /// <summary>
+    /// THis is Interface class for Account Manager
+    /// </summary>
     public interface IAccountManager
     {
+        /// <summary>
+        /// This is login 
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         Task<string> Login(LoginModel loginModel);
+        /// <summary>
+        /// This is for reset the password
+        /// </summary>
+        /// <param name="resetPassword"></param>
+        /// <returns></returns>
         Task<bool> ResetPassword(ResetPassword resetPassword);
+        /// <summary>
+        /// This is for Forget Password
+        /// </summary>
+        /// <param name="forgotPasswordModel"></param>
+        /// <returns></returns>
         Task<bool> ForgotPassword(ForgotPasswordModel forgotPasswordModel);
+        /// <summary>
+        /// Checks the password
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         Task<bool> CheckPassword(string email, string password);
+        /// <summary>
+        /// For Logout
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         string Logout(LoginModel loginModel);
+        /// <summary>
+        /// For google Login
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         Task<string> GoogleLogin(LoginModel loginModel);
+        /// <summary>
+        /// for Facebook Login
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         Task<string> FacebookLogin(LoginModel loginModel);
     }
 }
