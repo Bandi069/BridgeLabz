6288a3ff35d66b3ed56886705a2d91c3fb3fa6ba
Bandi069 <bandivenu89@gmail.com>
2020-03-11 12:48:27 +0530

    [Venu]Refactor.Implementation of AccountManager class in Manager

---
+++ Fundoo/Manager/Manager/AccountManager.cs
@@ -0,0 +1,57 @@
+﻿using Manager.InterfaceManager;
+using Model.UserModel;
+using Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Manager
+{
+    public class AccountManager : IAccountManager
+    {
+        private readonly IRepositoryuser repositoryuser;
+        public AccountManager()
+        {
+
+        }
+        public AccountManager(IRepositoryuser repositoryuser)
+        {
+            this.repositoryuser = repositoryuser;
+        }
+        public Task<bool> CheckPassword(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> FacebookLogin(LoginModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ForgotPassword(ForgotPasswordModel forgotPasswordModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GoogleLogin(LoginModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Login(LoginModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Logout(LoginModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ResetPassword(ResetPassword resetPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
