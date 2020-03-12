9d1684c8bcdd37153791b4b547310c1b34ddb6c4
Bandi069 <bandivenu89@gmail.com>
2020-03-12 17:19:00 +0530

    [Venu]Refactor.Implementation INoteManager

--- Fundoo/Manager/InterfaceManager/INoteManager.cs
+++ Fundoo/Manager/InterfaceManager/INoteManager.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Manager.InterfaceManager
 {
+    /// <summary>
+    /// INote Manager Interface
+    /// </summary>
     public interface INoteManager
     {
+        /// <summary>
+        /// This is Add Note Task
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns></returns>
         Task<string> AddNote(Notemodel noteModel);
+        /// <summary>
+        /// This is Delete Note Task
+        /// </summary>
+        /// <param name="NoteID"></param>
+        /// <returns></returns>
         Task<string> DeleteNote(int NoteID);
+        /// <summary>
+        /// This is Update Note Task
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns></returns>
         Task<string> UpdateNote(Notemodel noteModel);
+        /// <summary>
+        /// This is Get Note Task 
+        /// </summary>
+        /// <param name="NoteID"></param>
+        /// <returns></returns>
         List<Notemodel> GetNote(int NoteID);
 
     }
