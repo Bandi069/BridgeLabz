92cf862873fdd1705e33225478f5c509c16242ca
Bandi069 <bandivenu89@gmail.com>
2020-03-12 17:07:45 +0530

    [Venu]Refactor.Implementation NoteManager Class in Manager

--- Fundoo/Manager/Manager/NoteManager.cs
+++ Fundoo/Manager/Manager/NoteManager.cs
@@ -1,12 +1,63 @@
 ﻿using Manager.InterfaceManager;
+using Model.NoteModel;
+using Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.Manager
 {
     public class NoteManager : INoteManager
     {
+        private readonly INoteRepository noteRepository;
+        public NoteManager(INoteRepository noteRepository)
+        {
+            this.noteRepository = noteRepository;
+        }
+        public async Task<string> AddNote(Notemodel noteModel)
+        {
+            try
+            {
+                 await this.noteRepository.AddNote(noteModel);
+                return "Note Added";
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
+        }
 
+        public async Task<string> DeleteNote(int NoteID)
+        {
+            try
+            {
+                await this.noteRepository.DeleteNote(NoteID);
+                return "Note Deleted";
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public Task<string> getNote(int NoteID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> UpdateNote(Notemodel noteModel)
+        {
+            try
+            {
+                await this.noteRepository.UpdateNote(noteModel);
+                return "Note Updated";
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
