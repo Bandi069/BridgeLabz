7e41ac6eb1c4ca6424822c1b16d4ed3a63d8ff7f
Bandi069 <bandivenu89@gmail.com>
2020-03-12 15:35:13 +0530

    [Venu]Refactor.Implementation of AddNote task in Repository

--- Fundoo/Model/NoteModel/Notemodel.cs
+++ Fundoo/Model/NoteModel/Notemodel.cs
@@ -8,27 +8,29 @@ namespace Model.NoteModel
     public class Notemodel
     {
         private string noteID;
-        private string pinNote;
+        private bool pinNote;
         private string archive;
         private string description;
         private string title;
         private string addImg;
         private string remainder;
         private string addColor;
-        private string trash;
+        private bool trash;
         private string emailid;
         [Key]
         public string NoteID { get => noteID; set => noteID = value; }
+        public string Emailid { get => emailid; set => emailid = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
         public string Archive { get => archive; set => archive = value; }
         public string AddImg { get => addImg; set => addImg = value; }
         public string Remainder { get => remainder; set => remainder = value; }
-        public string PinNote { get => pinNote; set => pinNote = value; }
+        public bool PinNote { get => pinNote; set => pinNote = value; }
         public string AddColor { get => addColor; set => addColor = value; }
-        public string Trash { get => trash; set => trash = value; }
-        public string Emailid { get => emailid; set => emailid = value; }
+        public bool Trash { get => trash; set => trash = value; }
+        
         public DateTime? CreateTime { get; set; }
         public DateTime? ModifiedTime { get; set; }
-        public string Title { get => title; set => title = value; }
-        public string Description { get => description; set => description = value; }
+     
     }
 }
--- Fundoo/Repository/Repository/NoteRepository.cs
+++ Fundoo/Repository/Repository/NoteRepository.cs
@@ -17,22 +17,36 @@ namespace Repository.Repository
         }
         public Task AddNote(Notemodel noteModel)
         {
+            Notemodel notemodel = new Notemodel()
+            {
+                Emailid = noteModel.Emailid,
+                Title = noteModel.Title,
+                Description = noteModel.Description,
+                CreateTime = noteModel.CreateTime,
+                ModifiedTime = noteModel.ModifiedTime,
+                AddImg = null,
+                AddColor = null,
+                PinNote = false,
+                Remainder = null, 
+                Archive = null,
+                Trash=false
+            };
             return null;
         }
 
         public Task DeleteNote(int NoteID)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public List<Notemodel> GetNote(int NoteID)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public Task UpdateNote(Notemodel notemodel)
         {
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
