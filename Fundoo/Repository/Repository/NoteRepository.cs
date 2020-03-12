e9cebd800d2ee8fdbc0f61bee45233341011085b
Bandi069 <bandivenu89@gmail.com>
2020-03-12 15:24:37 +0530

    [Venu]Refactor.Implementation of NoteRepository

--- Fundoo/Repository/Repository/NoteRepository.cs
+++ Fundoo/Repository/Repository/NoteRepository.cs
@@ -1,12 +1,38 @@
-﻿using Repository.IRepository;
+﻿using Model.NoteModel;
+using Repository.IRepository;
+using Repository.UserDbContext;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Repository.Repository
 {
-  public class NoteRepository : INoteRepository
+    public class NoteRepository : INoteRepository
     {
+        private readonly UserContext userContext;
+        public NoteRepository(UserContext userContext)
+        {
+            this.userContext = userContext;
+        }
+        public Task AddNote(Notemodel noteModel)
+        {
+            return null;
+        }
 
+        public Task DeleteNote(int NoteID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notemodel> GetNote(int NoteID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateNote(Notemodel notemodel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
