e8cb2e4f6a3f0288f264c6018a71f2d0b326fcb6
Bandi069 <bandivenu89@gmail.com>
2020-03-12 17:24:32 +0530

    [Venu]Refactor.Implementation of NoteManager

--- Fundoo/Manager/Manager/NoteManager.cs
+++ Fundoo/Manager/Manager/NoteManager.cs
@@ -8,27 +8,45 @@ using System.Threading.Tasks;
 
 namespace Manager.Manager
 {
+    /// <summary>
+    /// This is Note Manager Class
+    /// </summary>
     public class NoteManager : INoteManager
     {
+        /// <summary>
+        /// This is INoteRepository  
+        /// </summary>
         private readonly INoteRepository noteRepository;
+        /// <summary>
+        /// constructor of notemanager dependency injection
+        /// </summary>
+        /// <param name="noteRepository"></param>
         public NoteManager(INoteRepository noteRepository)
         {
             this.noteRepository = noteRepository;
         }
+        /// <summary>
+        /// Async AdddNote Task
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns></returns>
         public async Task<string> AddNote(Notemodel noteModel)
         {
             try
             {
-                 await this.noteRepository.AddNote(noteModel);
+                await this.noteRepository.AddNote(noteModel);
                 return "Note Added";
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
-            
         }
-
+        /// <summary>
+        /// Delete noteAsyn task
+        /// </summary>
+        /// <param name="NoteID"></param>
+        /// <returns></returns>
         public async Task<string> DeleteNote(int NoteID)
         {
             try
@@ -41,26 +59,33 @@ namespace Manager.Manager
                 throw new Exception(e.Message);
             }
         }
-
+        /// <summary>
+        /// THis is to get note by NoteID
+        /// </summary>
+        /// <param name="NoteID"></param>
+        /// <returns></returns>
         public List<Notemodel> GetNote(int NoteID)
         {
             try
             {
                 var notelist = new List<Notemodel>();
                 var getlist = this.noteRepository.GetNote(NoteID);
-              foreach(var nlist in getlist)
+                foreach (var nlist in getlist)
                 {
                     notelist.Add(nlist);
                 }
                 return notelist;
-               
             }
             catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
         }
-
+        /// <summary>
+        /// This is asynchronous Update Note Task
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns></returns>
         public async Task<string> UpdateNote(Notemodel noteModel)
         {
             try
