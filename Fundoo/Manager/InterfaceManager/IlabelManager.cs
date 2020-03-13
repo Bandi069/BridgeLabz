116c1875f2a7be732643ad758fb77c7a326caf69
Bandi069 <bandivenu89@gmail.com>
2020-03-13 09:53:17 +0530

    [Venu]Add.Adding ILabelManager and LabelManager Class

---
+++ Fundoo/Manager/InterfaceManager/IlabelManager.cs
@@ -0,0 +1,15 @@
+﻿using Model.Label;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.InterfaceManager
+{
+   public interface IlabelManager
+    {
+        string AddLabel(LabelModel labelModle);
+        string DeleteLabel(int LabelID);
+        string UpdateLabel(LabelModel labelModel);
+        List<LabelModel> GetLabelModels(int LabelID);
+    }
+}
---
+++ Fundoo/Manager/Manager/LabelManager.cs
@@ -0,0 +1,38 @@
+﻿using Manager.InterfaceManager;
+using Model.Label;
+using Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Manager
+{
+    public class LabelManager : IlabelManager
+    {
+        private readonly ILabelRepository labelRepository;
+        public LabelManager(ILabelRepository labelRepository)
+        {
+            this.labelRepository = labelRepository;
+        }
+        public string AddLabel(LabelModel labelModle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DeleteLabel(int LabelID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LabelModel> GetLabelModels(int LabelID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateLabel(LabelModel labelModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
