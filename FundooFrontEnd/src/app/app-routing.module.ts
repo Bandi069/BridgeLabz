0eb65c76b979e3631c47d0ed8605090aa0612a5f
Bandi069 <bandivenu89@gmail.com>
2020-04-09 00:35:48 +0530

    [BandiVenu]Add.Adding Fundoo Front End Project File

---
+++ FundooFrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './Component/register/register.component';
+import { CommonModule } from '@angular/common';
+import {LoginComponent} from './Component/login/login.component';
+import { ForgotPasswordComponent }  from './Component/forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './Component/reset-password/reset-password.component';
+
+const routes: Routes = [
+  { path :'register',component:RegisterComponent},
+  {path : 'login', component: LoginComponent},
+  {path : 'forgot', component: ForgotPasswordComponent},
+   {path : 'resetpassword', component: ResetPasswordComponent},
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+        RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
